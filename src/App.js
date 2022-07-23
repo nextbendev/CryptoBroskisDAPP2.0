e19e7057052b08aa3f8b943a2018a5d6e205ab7c
Ben <nextbendev@gmail.com>
2022-07-23 11:14:39 -0400

    web3Incoming

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.png';
-import './App.css';
+import React, {Component} from "react";
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+      super(props)
+      this.state = {
+        account:'0x00'
+      }
+
+  }
+    render() {
+        return (
+          <div>
+            <Navbar account={this.state.account}/>
+            <div className='text-center'>
+              <h1></h1>
+            </div>
+          </div>
+        )
+    }
 }
+
+
 
 export default App;
